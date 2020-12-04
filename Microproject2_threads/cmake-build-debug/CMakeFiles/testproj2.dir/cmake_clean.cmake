7eba11afd85bfb718634839b80f71be8e08c60e5
Anas Ben Mustafa <57250290+arabianprince23@users.noreply.github.com>
2020-12-04 18:49:57 +0300

    Added Microproject2_threads

---
+++ Microproject2_threads/cmake-build-debug/CMakeFiles/testproj2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testproj2.dir/main.cpp.o"
+  "testproj2"
+  "testproj2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testproj2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
